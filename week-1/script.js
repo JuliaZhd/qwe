647c0975d7aef46a74108eecd887cd2262add6e1
JuliaZhd <fastomama@gmail.com>
2018-02-10 15:10:58 +0200

    изменить шрифт р

--- week-1/script.js
+++ week-1/script.js
@@ -1,1 +1,17 @@
-console.log('jfdkhgkjfgks');
+fs.onchange = function() {
+    if (isNaN(this.value)||(this.value<7)||(this.value>24)||(this.value % 1>0)) { // введено не число
+      // показать ошибку
+      //this.className = "error";//
+      alert ('Вы ввели не число или число вне диапазона от 8 до 24. Исправьте, пожалуйста.');
+    }
+    else {for (var i=0; i<document.getElementsByTagName('p').length; i++) {
+      document.getElementsByTagName('p')[i].style.fontSize = this.value+ 'px'}
+    };
+};
+  
+  /*fs.onfocus = function() {
+    if (this.className == 'error') { // сбросить состояние "ошибка", если оно есть
+      this.className = "";
+      error.innerHTML = "";
+    }
+  };*/
